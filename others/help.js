0f6171c19eec2b812ceef57bfc195f0f9630329e
Hyper <80406549+xdhyper@users.noreply.github.com>
2021-06-04 18:21:59 +0530

    Update help.js

--- others/help.js
+++ others/help.js
@@ -16,10 +16,10 @@ module.exports = {
     let commands = message.client.commands.array();
  
     let helpEmbed = new MessageEmbed()
-      .setTitle("Harmony Help")
-      .setAuthor('Click here to invite me!!', 'https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif', 'https://discord.com/api/oauth2/authorize?client_id=767885987740254291&permissions=49572160&scope=bot')
+      .setTitle("Hyper Help")
+      .setAuthor('Click here to invite me!!', 'https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif', 'https://discord.com/api/oauth2/authorize?client_id=815986498779217950&permissions=49572160&scope=bot')
       .setDescription(`**PREFIX:** \`${PREFIX}\``)
-      .setFooter( client.user.username +`Type: ${PREFIX}help <Command>  for more information!`, "https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif")
+      .setFooter( client.user.username + `Type: ${PREFIX}help <Command>  for more information!`, "https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif")
       .setColor("#F0EAD6");
 
       let ifargstruedothis = -1;
